fc1ecdf173e927a557c58201248a251ac2e70b00
Manomania <34182052+Manomania@users.noreply.github.com>
2025-10-17 21:54:29 +0200

    Add JSON structure for C++ pointer questions

---
+++ pointers.js
@@ -0,0 +1,12 @@
+{
+  "cards": [
+    {
+      "question": "Qu'est-ce qu'un pointeur en C++ ?",
+      "reponse": "Un pointeur est une variable qui stocke l'adresse mémoire d'une autre variable."
+    },
+    {
+      "question": "Comment déclarer un pointeur ?",
+      "reponse": "<code>int* ptr;</code> ou <code>int *ptr;</code><br><br>Le <code>*</code> indique que c'est un pointeur."
+    }
+  ]
+}
